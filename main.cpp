13780415da04f31a8a14b3f3209f85eaccdeccab
J00rdan <35407512+J00rdan@users.noreply.github.com>
2018-11-19 19:41:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <strings.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+
+int main(){
+
+    char x[21];
+    cin >> x;
+    for(int i = 0; i < strlen(x); i ++) x[i] = tolower(x[i]);
+    for(int i = 0; i < strlen(x); i ++) cout << x[i];
+}
